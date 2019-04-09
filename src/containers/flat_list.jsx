5485893d89a8dfe5cd9963afdb58db52e0fb0d15
Adam Sinning <asinning25@gmail.com>
2019-04-09 20:45:02 -0300

    flat list and map containers created

---
+++ src/containers/flat_list.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class FlatList extends Component {
+  render() {
+    return (
+      <div className="flat-list col-sm-7">
+        TODO Flat List
+      </div>
+    );
+  }
+}
+
+
+export default FlatList;
