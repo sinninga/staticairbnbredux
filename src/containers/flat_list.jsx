97c8440d8a85d5b76064e1297ff9e498f8b3386b
Adam Sinning <asinning25@gmail.com>
2019-04-09 21:41:05 -0300

    card added

--- src/containers/flat_list.jsx
+++ src/containers/flat_list.jsx
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import Flat from '../components/flat';
 
 class FlatList extends Component {
+  static defaultProps = {
+ flats: [{
+   "name": "Charm at the Steps of Montmartre",
+   "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat1.jpg",
+   "price": 164,
+   "priceCurrency": "EUR"
+   }]
+ }
+
   render() {
     return (
       <div className="flat-list col-sm-7">
-        TODO Flat List
+        {this.props.flats.map((flat) => <Flat key={flat.name} flat={flat} />)}
       </div>
     );
   }
