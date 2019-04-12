02913d8161520c6a9b980af7c5f3c3dd2d1bea69
Adam Sinning <asinning25@gmail.com>
2019-04-12 17:47:50 -0300

    redux dev tools added

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers, applyMiddleware  } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { logger } from 'redux-logger';
 import reduxPromise from 'redux-promise';
 
@@ -18,13 +18,14 @@ const reducers = combineReducers({
  selectedFlat: selectedFlatReducer
 });
 
-const middlewares = applyMiddleware(reduxPromise, logger);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const middlewares = composeEnhancers(applyMiddleware(reduxPromise, logger));
 
 import App from './components/app';
 
 const root = document.getElementById('root');
   ReactDOM.render(
-    <Provider store={createStore(reducers, {}, middlewares)}>
+    <Provider store={createStore(reducers, { flats: [] }, middlewares)}>
       <App />
     </Provider>, root);
 
